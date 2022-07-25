8f7eb2761bb7afdf7e49ed7060a201356f04dc3d
PavelK <Pakoryagin@ya.ru>
2022-07-25 12:56:12 +0800

    Проект калькулятор

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+const title = document.getElementsByTagName('h1')[0] ;
+
+const startBtn = document.getElementsByClassName('handler_btn')[0];
+const resetBtn = document.getElementsByClassName('handler_btn')[1];
+
+const buttonPlus = document.querySelector('.screen-btn');
+
+const otherItemsPercent = document.querySelectorAll('.other-items.percent');
+const otherItemsNumber = document.querySelectorAll('.other-items.number');
+
+const inputRange = document.querySelector('.rollback input');
+const inputRangeValue = document.querySelector('.rollback .range-value');
+
+const total = document.getElementsByClassName('total-input')[0];
+const totalCount = document.getElementsByClassName('total-input')[1];
+const totalCountOther = document.getElementsByClassName('total-input')[2];
+const totalFullCount = document.getElementsByClassName('total-input')[3];
+const totalCountRollback = document.getElementsByClassName('total-input')[4];
+
+let divScreen = document.querySelectorAll('.screen');
+
+
+console.log(title);
+console.log(startBtn);
+console.log(resetBtn);
+console.log(buttonPlus);
+console.log(otherItemsPercent);
+console.log(otherItemsNumber);
+
+console.log(inputRange);
+console.log(inputRangeValue);
+console.log(total);
+console.log(totalCount);
+console.log(totalCountOther);
+console.log(totalFullCount);
+console.log(totalCountRollback);
+console.log(divScreen);
+
+
+
+
